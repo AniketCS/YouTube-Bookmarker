5fe0cbcfedf3f0aa94330f429c0f3726e354b2ea
Aniket Mukherjee <an469681@dal.ca>
2024-09-22 03:09:00 -0300

    Identifying non youtube pages and completing viewing bookmarks function

--- popup.js
+++ popup.js
@@ -1,9 +1,24 @@
-import { getCurrentTab } from "./utils";
+import { getCurrentTab } from "./utils.js";
 
-// adding a new bookmark row to the popup
-const addNewBookmark = () => {};
+const addNewBookmark = (bookmarksElement, bookmark) => {
+    
+};
 
-const viewBookmarks = () => {};
+const viewBookmarks = (currentBookmarks =[]) => {
+    const bookmarksElement = document.getElementById("bookmarks");
+    bookmarksElement.innerHTML = "";
+
+    if (currentBookmarks.length > 0){
+        for(let i=0; i<currentBookmarks.length; i++){
+            const bookmark = currentBookmarks[i];
+            //adding one bookmark at a time and call the function every time we are adding a bookmark
+            addNewBookmark(bookmarksElement, bookmark);
+        }
+    }
+    else{
+        bookmarksElement.innerHTML = '<i class="row"> No bookmarks to show</i>';
+    }
+};
 
 const onPlay = e => {};
 
@@ -11,4 +26,26 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    const currentTab = await getCurrentTab();
+    const queryParameters = currentTab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    const currentVideo =  urlParameters.get("v");
+
+    //the Youtube video's unique identifier in the URL is used to storage bookmarks in the chrome storage, and so will also search using it
+    if (currentTab.url.includes("youtube.com/watch") && currentVideo) {
+        chrome.storage.sync.get([currentVideo], (data) => {
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
+
+            //view bookmarks
+
+        })
+    } 
+    else {
+        const container = document.getElementsByClassName("container")[0];
+
+        container.innerHTML = '<div class="title"> This is not a youtube video page.</div>';
+    }
+
+});
