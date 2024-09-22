f2e265521e1e130f0a296067d8290300f9682841
Aniket Mukherjee <an469681@dal.ca>
2024-09-22 01:06:10 -0300

    getting youtube left controls and videostream

--- background.js
+++ background.js
@@ -10,8 +10,29 @@ chrome.tabs.onUpdated.addListener((tabId, tab)=>{
       type: "NEW",
       videoId: urlParameters.get("v"),
     
-
     });
+
+    const newVideoLoaded = () => {
+      const bookmarkButtonExists = document.getElementsByClassName("bookmark-btn")[0];
+      
+      console.log(bookmarkButtonExists);
+
+      if (!bookmarkButtonExists){
+        const bookmarkButton = document.createElement("img");
+
+        bookmarkButton.src=chrome.runtime.getURL("assets/bookmark.png");
+        bookmarkButton.className = "youtube-button" + "bookmark-button";
+        bookmarkButton.title = "Click to bookmark the current timestamp";
+
+        youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+
+        youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+        youtubeLeftControls.appendChild(bookmarkButton);
+
+        
+      }
+    }
 
 
 
