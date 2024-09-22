0e7b24176363f68bd6c8aabf66b43cf082a5f694
Aniket Mukherjee <an469681@dal.ca>
2024-09-22 02:45:05 -0300

    Getting chrome current tab in utils and importing in popup

---
+++ utils.js
@@ -0,0 +1,8 @@
+//Code snippet taken from https://developer.chrome.com/docs/extensions/reference/api/tabs for getting current tab on chrome
+
+export async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
