f00912cb5bd89f0dc8b866dafc8b95d2bacd7c46
Aniket Mukherjee <an469681@dal.ca>
2024-09-22 00:27:25 -0300

    Getting url search params

--- background.js
+++ background.js
@@ -1,17 +1,20 @@
 chrome.tabs.onUpdated.addListener((tabId, tab)=>{
 
-  if(tab.url && tab.url.includes("youtube.com/watch")){
+  if(changeInfo.status === 'complete' && tab.url && tab.url.includes("youtube.com/watch")) {
 
     const queryParameters = tab.url.split("?")[1];
-    const urlParams = new URLSearchParams(queryParameters);
+    const urlParameters = new URLSearchParams(queryParameters);
+    console.log(urlParameters);
 
     chrome.tabs.sendMessage(tabId, {
       type: "NEW",
-      videoId: urlParams.get("v")
+      videoId: urlParameters.get("v"),
+    
+
     });
 
-    
+
 
 
   }
-})
+});
--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,16 @@
 (() =>{
 let youtubeLeftControls, youtubePlayer;
+let currentVideo= "";
 
-})
-();
+//listen to background.js message
+
+chrome.runtime.onMessage.addListener ((obj, sender, response) =>{
+    const {type, value, videoId} =obj;
+
+    if(type === "NEW") {
+        currentVideo =videoId;
+        newVideoLoaded();
+    }
+});
+
+})();
